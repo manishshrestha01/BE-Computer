704e2cb5279d9cda3032f870345a4a1f2c9cf88e
Manish <manish@Manishs-MacBook-Air.local>
2024-07-20 22:08:50 +0545

    NOTES

---
+++ Semester 4/NM LAB TESTING/nr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#define e 0.001
+#define f(x) pow(x,2)-3*x+3
+#define df(x) 2*x-3
+
+int main(){
+    float x0,x1,f0,f1,df0;
+    int i=0;
+  printf("enter the value of x0:");
+  scanf("%f",&x0);
+    do
+    {
+        f0=f(x0);
+        df0=df(x0);
+        x1=x0-(f0/df0);
+        f1=f(x1);
+        x0=x1;
+        i++;
+        printf("no. of iteration=%d\t",i);
+        printf("root=%f\t",x1);
+        printf("function=%f\n",f1);
+    } while (fabs(f1)>e);
+    return 0;
+}
